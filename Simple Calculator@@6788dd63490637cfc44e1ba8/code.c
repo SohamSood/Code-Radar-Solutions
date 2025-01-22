5b2e6c3feaf33b3604f062e919f48b90858852b0
SohamSood <soodsoham86@gmail.com>
2025-01-22 19:11:06 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,12 +3,12 @@
 int main() {
     float a,b;
     char c;
-    scanf("%d %d %c",&a,&b,&c);
+    scanf("%f %f %c",&a,&b,&c);
     if( c == '+') {
         int u = a+b;
         printf("%d",u);
     } else if (c == '-') {
-        int u =(int)a-b;
+        int u =a-b;
         printf("%d",u);
     } else if(c == '*') {
         float e = a*b;
