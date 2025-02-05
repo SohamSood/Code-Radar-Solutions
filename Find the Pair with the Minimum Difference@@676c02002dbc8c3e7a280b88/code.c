4122f1ceef91a7123bcc6cda4e2f681f984a7ca2
SohamSood <soodsoham86@gmail.com>
2025-02-05 19:33:05 +0530

    Solution to Find the Pair with the Minimum Difference

---
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    int *myarr = malloc(a *(sizeof(int)));
+    for (int l = 0;l<a;l++) {
+        scanf("%d",&myarr[k]);
+    }
+    
+}
