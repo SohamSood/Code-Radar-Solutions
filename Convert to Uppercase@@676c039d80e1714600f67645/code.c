ec4ffb999dfb94867c21e9b87b2a557324f257e4
SohamSood <soodsoham86@gmail.com>
2025-02-27 19:24:51 +0530

    Solution to Convert to Uppercase

--- Convert to Uppercase@@676c039d80e1714600f67645/code.c
+++ Convert to Uppercase@@676c039d80e1714600f67645/code.c
@@ -4,7 +4,9 @@ int main() {
     char a[100];
     scanf("%s",&a);
     for(int i=0;i<strlen(a);i++) {
-        char k = a[i] -32;
-        printf("%c",k);
+            if(a[i]>a && a[i]<z) {
+            char k = a[i] -32;
+            printf("%c",k);
+        }
     }
 }
