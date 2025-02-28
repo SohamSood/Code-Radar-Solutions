a1f9e23893992302cf4c283ffbc678c15f2f9bac
SohamSood <soodsoham86@gmail.com>
2025-02-28 20:10:27 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    int *myarr = malloc(a*(sizeof(int)));
+    for(int p = 0;p<a;p++) {
+        scanf("%d",&myarr[p]);
+    }
+    for(int y = 0;y<a;y++) {
+        prime(myarr[y]);
+    }
+}
 void prime(int a) {
     if(a == 0 ||a == 1|| a == 2) {
         printf("1 \n");
@@ -15,16 +27,5 @@ void prime(int a) {
         } else {
             printf("0 \n");
         }
-    }
-}
-int main( ) {
-    int a;
-    scanf("%d",&a);
-    int *myarr = malloc(a*(sizeof(int)));
-    for(int p = 0;p<a;p++) {
-        scanf("%d",&myarr[p]);
-    }
-    for(int y = 0;y<a;y++) {
-        prime(myarr[y]);
     }
 }
