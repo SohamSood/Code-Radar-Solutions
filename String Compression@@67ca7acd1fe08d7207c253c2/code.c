75410ab8fa4edb7883cedc4a5c09dd5288f95cf4
SohamSood <soodsoham86@gmail.com>
2025-03-07 22:17:25 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -1,9 +1,9 @@
 // Your code here...
 int compressString(int *str,int *a) {
-    for(int i= 0;i<(str(len));i++) {
+    for(int i= 0;i<(strlen(str));i++) {
         int count = 0;
         int var = str[i];
-        for(int j =0;j<str(len);j++) {
+        for(int j =0;j<strlen(str);j++) {
             if(str[i] == str[j]) {
                 count++;
             }
