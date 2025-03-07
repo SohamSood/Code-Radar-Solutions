756c5e277cb7fd39a8499de5cf5d8cbc717a3402
SohamSood <soodsoham86@gmail.com>
2025-03-07 22:02:38 +0530

    Solution to Insertion Sort for Characters

--- Insertion Sort for Characters@@67c9a8aa6e98bd154cc82f42/code.c
+++ Insertion Sort for Characters@@67c9a8aa6e98bd154cc82f42/code.c
@@ -1,17 +1,18 @@
 // Your code here...
+// Your code here...
 int insertionSort(char *myarr,int a) {
-    for(int i = 0;i<a;i++) {
-        for(int j = 0;j<a;j++) {
-            if(myarr[i]<myarr[j]) {
-                int temp = myarr[j];
+    for(int i =0;i<a;i++) {
+        for(int j =0;j<a;j++) {
+            if(myarr[i] < myarr[j]) {
+                int vari = myarr[j];
                 myarr[j] = myarr[i];
-                myarr[i] = temp;
+                myarr[i] = vari; 
             }
         }
     }
 }
-int printArray(char *myarr;int a) {
-    for(int i=0;i<a;i++) {
-        printf("%d ",myarr[i]);
+int printArray(char *myarr,int a) {
+    for(int i =0;i<a;i++) {
+        printf("%c ",myarr[i]);
     }
 }
