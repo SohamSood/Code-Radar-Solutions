aa00eb7917ab89f5d8664ccb43628ad24db977c9
SohamSood <soodsoham86@gmail.com>
2025-03-08 21:50:17 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,22 +1,29 @@
 // Your code here...
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
-    int a;
-    scanf("%d",&a);
-    int *myarr = malloc(a*(sizeof(int)));
+int rotate(int *myarr,int a) {
     int *myarr2 = malloc(a*(sizeof(int)));
-    for(int i = 0;i<a;i++) {
-        scanf("%d",&myarr[i]);
-    }
     myarr2[0] = myarr[a-1];
     myarr2[a-1] = myarr[a-2];
     for(int i = 1;i<a-1;i++) {
         myarr2[i] = myarr[i+1];
     }
     for(int i = 1;i<a;i++) {
-        printf("%d \n",myarr2[i]);
+        myarr[i] = myarr2[i]
     }
 
+}
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    int *myarr = malloc(a*(sizeof(int)));  
+    for(int i = 0;i<a;i++) {
+        scanf("%d",&myarr[i]);
+    }
+    int c = 0;
+    scanf("%d",&c);
+    for(int i = 0;i<c;i++) {
+        rotate(myarr,a);
+    }
 }
