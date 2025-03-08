ceda5960d2b7c3cfd50c5e5c1009c24cfec0234d
SohamSood <soodsoham86@gmail.com>
2025-03-09 00:28:59 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+int bubbleSort(int *myarr,int a) {
+    for(int i =0;i<a;i++) {
+        for(int j=0;j<a;j++) {
+            if(myarr[i]<myarr[j]) {
+                int vari = myarr[j];
+                myarr[j] = myarr[i];
+                myarr[i] = vari;
+            }
+        }
+    }
+}
 int main() {
     int a,b;
-
     scanf("%d",&a);
     int *myarr = malloc(a*(sizeof(int)));
     for(int p=0;p<a;p++) {
         scanf("%d",&myarr[p]);
     }
     scanf("%d",&b);
-    for(int i =0;i<a;i++) {
-        for(int j = 0;j<a;j++) {
-            if((i!=j)) {
-                if((myarr[i] +myarr[j])==b) {
-                    printf("%d %d \n",myarr[i],myarr[j]);    
-                }
-            } 
+    bubbleSort(myarr,a);
+    for(int i = 0;i<a;i++) {
+        for(int j = i+1;j<a;j++) {
+            if(myarr[i] +myarr [j] == b) {
+                printf("%d %d",i,j);
+                printf("\n");
+            }
         }
     }
+    
 }
