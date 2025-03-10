67ec9e37bcdb01f5391b58be869413d1e02620f6
SohamSood <soodsoham86@gmail.com>
2025-03-11 00:15:27 +0530

    Solution to Create a Program to Store and Display Student Records

---
+++ Create a Program to Store and Display Student Records@@67877d61dc107486ad5c2742/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+    int no;
+    char name[100];
+    float marks;
+}
+int main() {
+    int a;
+    scanf("%d",&a);
+    struct student *myarr = (struct student*) malloc(a*(sizeof(struct student)));
+    for(int i = 0;i<a;i++) {
+        scanf("%d %s %f",&students[i].no,students[i].name,&students[i].marks)
+    }
+}
