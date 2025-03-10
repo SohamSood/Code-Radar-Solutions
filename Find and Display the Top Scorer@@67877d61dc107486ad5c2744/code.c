d0c402462a1414bd97bfd6ce3b25d1ce1aa869bc
SohamSood <soodsoham86@gmail.com>
2025-03-11 00:04:49 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -10,4 +10,7 @@ int main() {
     int c;
     scanf("%d",&c);
     struct student *students = (struct student *)malloc(c* sizeof(struct student));
+    for(int i= 0;i<c;i++) {
+        scanf("%d %s %f",&students[i].a,students[i].name,&students[i].b);
+    }
 }
