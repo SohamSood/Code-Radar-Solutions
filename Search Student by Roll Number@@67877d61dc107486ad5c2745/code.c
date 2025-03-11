876331bafb3b7d8e1e119fbbe94078a7666956df
SohamSood <soodsoham86@gmail.com>
2025-03-11 18:48:43 +0530

    Solution to Search Student by Roll Number

---
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+    int a;
+    char b[100];
+    float c;
+};
+int main() {
+    int d;
+    scanf("%d",&d);
+    struct student *students = (struct student*)malloc(d*sizeof(struct student));
+    for(int i = 0;i<d;i++) {
+        scanf("%d %s %f",&student[i].a,students[i].b,&students[i].c);
+    }
+}
