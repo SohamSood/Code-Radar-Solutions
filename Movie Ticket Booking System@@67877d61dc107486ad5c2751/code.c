f82d09c075373b2ce988e074ffb55e147854a6a0
SohamSood <soodsoham86@gmail.com>
2025-03-11 21:26:54 +0530

    Solution to Movie Ticket Booking System

---
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -0,0 +1,33 @@
+// Your code here...
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct student {
+    char no[100];
+    char name[100];
+    float marks;
+};
+int main() {
+    int a;
+    scanf("%d",&a);
+    struct student *students = (struct student*)malloc(a*sizeof(struct student));
+    for(int i = 0;i<a;i++) {
+        scanf("%s %s %f",students[i].no,students[i].name,&students[i].marks);
+    }
+    float car = 0;
+    float bike = 0;
+    float truck = 0;
+    for(int i = 0;i<a;i++) {
+        if((strcmp(students[i].name, "Standard")) == 0) {
+            car = car+students[i].marks;
+        }
+        else if((strcmp(students[i].name, "Premium")) == 0) {
+            bike = bike+students[i].marks;
+        }
+        else if((strcmp(students[i].name, "VIP")) == 0) {
+            truck = truck+students[i].marks;
+        }
+    }
+    printf("Standard: %.2f, Premium: %.2f, VIP: %.2f",car,truck,bike);
+}
