27372010f31c76b602795443a9532357b9d4b822
SohamSood <soodsoham86@gmail.com>
2025-03-12 19:37:36 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,38 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
+// Your code here...
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+int max(int arr[], int a){
+    int max = arr[0];
+    for(int i =0; i<a; i++){
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+int min(int arr[], int a){
+    int min = arr[0];
+    for(int i =0; i<a; i++){
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+int main(){
     int a;
-    scanf("%d",&a);
-    int *myarr = malloc(a*4);
-    for(int p = 0;p<a;p++) {
-        scanf("%d",&myarr[p]);
+    scanf("%d", &a);
+    int *myarray = malloc(a * sizeof(int));
+    for(int i =0; i<a; i++){
+        scanf("%d", &myarray[i]);
     }
-    int min=myarr[0];
-    for(int i =0;i<a;i++) {
-        if(min>myarr[i]) {
-            min = myarr[i];
+    int e = 0;
+    for(int i = 0;i<a;i++) {
+        if(myarray[i] == 1) {
+            e++;
         }
     }
-    int max=myarr[0];
-    for(int i =0;i<a;i++) {
-        if(max<myarr[i]) {
-            max = myarr[i];
-        }
-    }
-    for(int i = min;i<=max;i++) {
-        int e = 0;
-        for(int j = 0;j<a;j++) {
-            if(myarr[j] == i) {
-                printf("%d ",i);
-                e++;
+    if(e == 0) {
+        printf("1");
+    } else {
+        int maxx = max(myarray,a);
+        int minn = min(myarray,a);
+        int f = 0;
+        for(int i = minn;i<=maxx;i++) {
+            int e = 0;
+            for(int j = 0;j<a;j++) {
+                if((myarray[j] == i) && (myarray[j] >0)) {
+                    e++ ;
+                }
+            }
+            if ((e == 0) & (i>0)) {
+                printf("%d",i);
+                f++;
             }
         }
-        if(e == 0) {
-            printf("%d ",i);
-
-        } else {
-            int r=0;
-            // printf("%d ",e);
+        if(f == 0) {
+            printf("%d",maxx+1);
         }
     }
 }
