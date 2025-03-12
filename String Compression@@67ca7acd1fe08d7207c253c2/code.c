bf988d36b765025ff7f99c46591e06e4d64fe134
SohamSood <soodsoham86@gmail.com>
2025-03-12 22:41:54 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -1,24 +1,37 @@
-// Your code here...
-int compressString(char *myarr,char *myarr2) {
+int compressString(char *myarr, char *myarr2) {
     int a = 0;
     while (myarr[a] != '\0') {
         a++;
     }
     int index = 0;
-    for(int i = 0;i<a;i++) {
+    for (int i = 0; i < a; i++) {
         int count = 0;
-        for(int j = 0;j<a;j++) {
-            if(myarr[i] == myarr[j]) {
+        for (int j = 0; j < a; j++) {
+            if (myarr[i] == myarr[j]) {
                 count++;
             }
         }
-        if(count == 1) {
-            myarr2[index] = myarr[i];
-            index++;
-        } else {
-            myarr2[index] = myarr[i];
-            myarr2[index+1] = count + '0';
-            index = index+2;
+        int b = 0;
+        while (myarr2[b] != '\0') {
+            b++;
+        }
+        int c = 0;
+        for(int k = 0;k<b;k++) {
+            if(i == myarr2[k]) {
+                c++;
+            }
+        }
+
+
+        if (c == 0) {
+            if (count == 1) {
+                myarr2[index] = myarr[i];
+                index++;
+            } else {
+                myarr2[index] = myarr[i];
+                myarr2[index + 1] = count + '0';
+                index = index + 2;
+            }
         }
     }
 }
