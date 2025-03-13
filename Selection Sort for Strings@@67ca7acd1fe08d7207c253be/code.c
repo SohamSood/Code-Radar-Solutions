280d062e3c78c9b6f4027a94c4ea6444e434b9cb
SohamSood <soodsoham86@gmail.com>
2025-03-13 13:56:32 +0530

    Solution to Selection Sort for Strings

---
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -0,0 +1,9 @@
+// Your code here...
+int printarray(char *myarr,int a) {
+    for(int i = 0;i<a;i++) {
+        printf("%s \n",myarr[i]);
+    }
+}
+int selectionsort(char *myarr,int a) {
+    int e =0;
+}
