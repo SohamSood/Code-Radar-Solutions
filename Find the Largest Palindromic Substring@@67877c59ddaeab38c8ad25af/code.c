a3cc18b15c43d660e24966b72d99fa3c868f41cb
SohamSood <soodsoham86@gmail.com>
2025-03-13 23:29:32 +0530

    Solution to Find the Largest Palindromic Substring

---
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stype.h>
+int pallindrome(char *myarr,int a) {
+    char *myarr[a];
+    for(int i = 0;i<a;i++) {
+        myarr2[i] = myarr2[a-1-i];
+    }
+    printf("%s",myarr2);
+}
+int main() {
+    char myarr[100];
+    fgets(myarr,100,stdin);
+    pallindrome(myarr);
+}
