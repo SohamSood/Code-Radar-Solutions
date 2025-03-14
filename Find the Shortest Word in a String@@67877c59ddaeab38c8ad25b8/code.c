01e442de756f0c94dd47b35053da6afbbaa85429
SohamSood <soodsoham86@gmail.com>
2025-03-14 12:47:39 +0530

    Solution to Find the Shortest Word in a String

--- Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
+++ Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
@@ -8,7 +8,7 @@ int main() {
     int maxx = 0;
     for(int j = 0;j<a;j++) {
         int count = 0;
-        if ((myarr[j] != " ") && (myarr[j-1] = " ")) {
+        if ((myarr[j] != ' ') && (myarr[j-1] == ' ')) {
             for(int i = j;i<a;i++) {
                 if(myarr[i] == ' ') {
                     break;
