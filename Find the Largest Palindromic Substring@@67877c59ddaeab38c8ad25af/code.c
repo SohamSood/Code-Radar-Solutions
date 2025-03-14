74f6a6130550e4b9573a1c56c94e7bae68f596b6
SohamSood <soodsoham86@gmail.com>
2025-03-14 14:12:50 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -14,10 +14,43 @@ int pallindrome(char *myarr) {
         return 0;
     }
 }
+void longestPalindromicSubstring(char *str) {
+    int a = strlen(str);
+    int maxLen = 0, startIdx = 0;
+    char temp[100];  // Temporary substring storage
+
+    // Generate all substrings
+    for (int i = 0; i < a; i++) {
+        for (int j = i; j < a; j++) {
+            int index = 0;
+
+            // Extract substring
+            for (int k = i; k <= j; k++) {
+                temp[index++] = str[k];
+            }
+            temp[index] = '\0';  // Null-terminate substring
+
+            // Check if it's a palindrome using your function
+            if (pallindrome(temp)) {
+                int curLen = j - i + 1;
+                if (curLen > maxLen) {
+                    maxLen = curLen;
+                    startIdx = i;
+                }
+            }
+        }
+    }
+
+    // Print the longest palindrome
+    printf("Longest Palindromic Substring: ");
+    for (int i = startIdx; i < startIdx + maxLen; i++) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+}
 int main() {
     char myarr[100];
     fgets(myarr,100,stdin);
     int a = strlen(myarr);
-    int b = pallindrome(myarr);
-    printf("%d",b);
+    longestPalindromicSubstring(str);
 }
