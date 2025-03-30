5553cfa9392b2e448388dade5b42b226b0ebd324
SohamSood <soodsoham86@gmail.com>
2025-03-30 14:56:28 +0530

    Solution to Topper Tracker – Breaking Test Score Records

---
+++ Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
@@ -0,0 +1,18 @@
+int trackscores(int *myarr,int a,int *result) {
+    int max = myarr[0];
+    int min = myarr[0];
+    int countmax = 0;
+    int countmin = 0;
+    for(int i  = 0;i<a;i++) {
+        if(myarr[i]>max) {
+            max = myarr[i];
+            countmax++;
+        }
+        if(myarr[i]<min) {
+            min = myarr[i];
+            countmin++;
+        }
+    }
+    result[0] = countmax;
+    result[1] = countmin;
+}
